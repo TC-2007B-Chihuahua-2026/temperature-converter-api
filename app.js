ce3a52e914e4106ed1843d1c0d49fd72b2b1ac1f
Enrique Sanchez <enrike.sanchez@tec.mx>
2026-08-30 16:09:05 -0600

    issue-5 Added /api-docs path to the application for swagger

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./src/config/swagger');
 const temperatureRoutes = require('./src/routes/temperature.routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/', temperatureRoutes);
 
 app.listen(port, '0.0.0.0', () => {
