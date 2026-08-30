7fd50f0cb8ae057928078b579b52d748b9a79dc2
Enrique Sanchez <enrike.sanchez@tec.mx>
2026-08-30 15:14:50 -0600

    issue-3 Updated controller to invoke service

--- src/controllers/temperature.controller.js
+++ src/controllers/temperature.controller.js
@@ -2,22 +2,24 @@
  * Temperature request controller.
  */
 const TemperatureVO = require('../valueobjects/temperature.vo');
+const {
+  convertTemperature: convertTemperatureService,
+} = require('../services/temperature.service');
 
 /**
  * Handles temperature conversion requests.
  * @param {import('express').Request} req - Express request object.
  * @param {import('express').Response} res - Express response object.
- * @returns {import('express').Response} The TemperatureVO response.
+ * @returns {import('express').Response} The converted TemperatureVO response.
  */
 const convertTemperature = (req, res) => {
   const { value, unit } = req.body;
   const unitToConvert = req.params.unitToConvert;
 
-  console.log('unitToConvert:', unitToConvert);
-
   const temperature = new TemperatureVO(value, unit);
+  const convertedTemperature = convertTemperatureService(temperature, unitToConvert);
 
-  return res.status(200).json(temperature);
+  return res.status(200).json(convertedTemperature);
 };
 
 module.exports = {
