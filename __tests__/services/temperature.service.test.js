c8232c345f46f3b35f1c968e35b2808f171029a3
Enrique Sanchez <enrike.sanchez@tec.mx>
2026-08-30 14:36:01 -0600

    issue-3 Generate Unit Test for Celsius to Fahrenheit

---
+++ __tests__/services/temperature.service.test.js
@@ -0,0 +1,17 @@
+const TemperatureVO = require('../valueobjects/temperature.vo');
+const { convertTemperature } = require('../src/services/temperature.service');
+
+describe('Temperature service', () => {
+  test('GIVEN a Celsius temperature WHEN converted to Fahrenheit THEN it returns the converted value in Fahrenheit', () => {
+    // GIVEN
+    const celsiudTemperature = new TemperatureVO(23, 'CELSIUS');
+
+    // WHEN
+    const fahrenheitTemperature = convertTemperature(celsiudTemperature, 'FAHRENHEIT');
+
+    // THEN
+    expect(fahrenheitTemperature).toBeInstanceOf(TemperatureVO);
+    expect(fahrenheitTemperature.value).toBe(73.4);
+    expect(fahrenheitTemperature.unit).toBe('FAHRENHEIT');
+  });
+});
